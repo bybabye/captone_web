b83e1f56e28dc3c5f7ad5de689701c19b07e3ec4
bybabye <89548252+bybabye@users.noreply.github.com>
2024-01-03 11:59:32 +0700

    update change cloud server 03/01

--- src/utils/contants.jsx
+++ src/utils/contants.jsx
@@ -1,6 +1,6 @@
-//export const API_SERVER = "https://cap1.onrender.com";
+export const API_SERVER = "https://cap1.onrender.com";
 
-export const API_SERVER = "http://localhost:4000";
+// export const API_SERVER = "http://localhost:4000";
 
 
 //User
